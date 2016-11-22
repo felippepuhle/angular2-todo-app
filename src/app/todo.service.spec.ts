436a3b8f0f554d28408eb7bbfe851ad5662dbf80
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2016-11-22 21:36:22 -0200

    Exercices made following sitepoint's tutorial.

--- src/app/todo.service.spec.ts
+++ src/app/todo.service.spec.ts
@@ -1,12 +1,5 @@
 /* tslint:disable:no-unused-variable */
 
-import {
-  beforeEach, beforeEachProviders,
-  describe, xdescribe,
-  expect, it, xit,
-  async, inject
-} from '@angular/core/testing';
-
 import {Todo} from './todo';
 import {TodoService} from './todo.service';
 
